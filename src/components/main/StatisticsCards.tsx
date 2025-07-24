44fdee64536f311d78d83e1fee9eca84c3930e3e
ay-ub <ayoubhadjyoucef1@gmail.com>
2025-07-24 12:48:58 +0100

    style: make statistics cards responsive

--- src/components/main/StatisticsCards.tsx
+++ src/components/main/StatisticsCards.tsx
@@ -22,21 +22,24 @@ function StatisticsCards() {
       },
     ];
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 container mx-auto">
-        {
-            statisticsData.map((stat)=>{
-                return (
-                  <div key={stat.number} className="flex items-center gap-4 p-4 hover:shadow-2xl duration-300 rounded-md w-[264px] h-[128px]">
-                    <span className="text-6xl text-[#31A758] uppercase font-bold">
-                      {stat.number}
-                    </span>
-                    <span className="text-[#909090]">{stat.text}</span>
-                  </div>
-                );
-            })  
-        }
+    <div className=" container mx-auto">
+      <div className="flex justify-between items-center flex-wrap">
+        {statisticsData.map((stat) => {
+          return (
+            <div
+              key={stat.number}
+              className="flex items-center gap-4 p-4 hover:shadow-2xl duration-300 rounded-md w-full justify-center lg:w-[264px] lg:h-[128px]"
+            >
+              <span className="text-6xl text-[#31A758] uppercase font-bold">
+                {stat.number}
+              </span>
+              <span className="text-[#909090]">{stat.text}</span>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
 export default StatisticsCards
